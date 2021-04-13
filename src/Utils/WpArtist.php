9bf63a4b4229955f0f92bdcb8359ab9359995910
mokka-kevin <kevin@mokka-merch.com>
2021-04-13 19:02:43 +0200

    cleanup

--- src/Utils/WpArtist.php
+++ src/Utils/WpArtist.php
@@ -24,6 +24,7 @@ class WpArtist extends Artist
      * Get the artist name by provided product id.
      * It checks if the product is a variation and pulls term_ids from the parent product (simple product).
      *
+     * @since 0.2.7
      * @uses get_term_ids_for_product
      * @param int $productId product id
      * @return mixed array | null
@@ -33,7 +34,15 @@ class WpArtist extends Artist
     {
         global $wpdb;
         try {
+            $_product = wc_get_product($productId);
+            $is_variation = $_product->get_parent_id();
+            
+            if ($is_variation) {
+                $productId = $_product->get_parent_id();
+            }
+
             $artist_id = get_post_meta($productId, '_artist_id', true);
+            
     
             if (empty($artist_id)) {
                 $term_ids = self::get_term_ids_for_product($productId);
@@ -45,14 +54,6 @@ class WpArtist extends Artist
                 $result = $wpdb->get_row($query, ARRAY_A);
             } else {
                 $_product_id = $productId;
-                $_product = wc_get_product($productId);
-                // Logger::write_log($_product);
-                $is_variation = $_product->get_parent_id();
-                
-                if ($is_variation) {
-                    $_product_id = $_product->get_parent_id();
-                    Logger::write_log("we are here");
-                }
                 $query = $wpdb->prepare("SELECT * FROM {$wpdb->prefix}artists WHERE id = %d", $artist_id);
                 $result = $wpdb->get_row($query, ARRAY_A);
             }
@@ -61,29 +62,32 @@ class WpArtist extends Artist
     
             return $result;
         }
-        catch(Exception $e) {
+        catch(\Exception $e) {
             Logger::write_log($e->getMessage());
         }
-
     }
 
-    public static function get_artist_id_from_product(int $productId, $child = false) :string
+    /**
+     * get_all_product_ids_by_artist_number
+     * 
+     * Due to old implementation we are using artist_nummer (yes - it's denglish)
+     * Only returns main products (no variations)
+     * 
+     * @since 0.2.8
+     * @return mixed array | null
+     */
+    public static function get_all_product_ids_by_artist_number(int $artist_id) 
     {
-        global $wpdb;
-
-        $term_ids = self::get_term_ids_for_product($productId);
-        $in_array_sql = implode("', '", $term_ids);
-
-        $query = "SELECT * FROM {$wpdb->prefix}artists WHERE term_id IN ('$in_array_sql')";
-        if ($child) $query .= " AND parent_id <> '0'";
-        if (!$child) $query .= " AND parent_id = 0";
-        $result = $wpdb->get_row($query, ARRAY_A);
-
-        if (!isset($result['artist_name'])) return '';
-
-        return $result["artist_name"];
+        try {
+            global $wpdb;
+            $product_ids = $wpdb->get_col($wpdb->prepare("SELECT post_id FROM {$wpdb->prefix}postmeta WHERE meta_key = '_artist_id' AND meta_value = %d", $artist_id));
+            return $product_ids;
+        }
+        catch(\Exception $e) {
+            Logger::write_log($e->getMessage());
+        }
     }
-    ///
+
     /**
      * gets all registered term_ids by product_id
      *
