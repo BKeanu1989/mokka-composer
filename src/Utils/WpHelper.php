4809d900859ad6cd41d8db1a6b6df89c3d63167f
mokka-kevin <kevin@mokka-merch.com>
2021-04-13 19:01:14 +0200

    + pullOut

--- src/Utils/WpHelper.php
+++ src/Utils/WpHelper.php
@@ -24,4 +24,28 @@ class WpHelper
         $str = $name .'_'. $function;
         return $str;
     }
+
+    /**
+     * Pulls only a given set out of an assosiative array.
+     * 
+     * $data - whatever associative array is pulled from db.
+     * $whiteListed - the whiteList. Or to say it in non offending way to some social justice warriors. The list which u want.
+     * $lookFor =   
+     * 
+     * @since 0.3.0
+     * 
+     */
+    public static function pullOut($data, array $whiteListed = [], string $lookFor = 'meta_value', string $getValue = 'meta_key') {
+        $returnArray = [];
+        $formatted_data = array_column($data, $lookFor, $getValue);
+    
+        foreach($whiteListed AS $key) {
+            if (isset($formatted_data[$key])) {
+                $returnArray[$key] = $formatted_data[$key];
+                continue;
+            }
+            $returnArray[$key] = '';
+        }
+        return $returnArray;
+    }
 }
