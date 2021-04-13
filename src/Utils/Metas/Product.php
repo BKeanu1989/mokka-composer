8d289ae9889a4647fa06975241482c4c2279116c
mokka-kevin <kevin@mokka-merch.com>
2021-04-13 19:00:04 +0200

    init - might not be used

---
+++ src/Utils/Metas/Product.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Mokka\Utils\Metas;
+
+class Product
+{
+    public function __construct()
+    {
+
+    }
+}
---
+++ src/Utils/Metas/Variation.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Mokka\Utils\Metas;
+
+class Variation
+{
+    public function __construct()
+    {
+        
+    }
+}
